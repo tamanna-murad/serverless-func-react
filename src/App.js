341341f2bbc3d46af1ffb51aaca35427c9cbbb54
Tamanna Murad <tamannamurad246810@gmail.com>
2022-08-14 13:52:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Basic from "./Basic";
+import Airtable from "./Airtable";
+function App() {
+  // return <h2>React and Serverless Functions</h2>
+  return <>
+    <Basic />
+    <Airtable />
+  </>
+}
+
+export default App
